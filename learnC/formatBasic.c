f57a36d44f6640f14c254bf3ceb8a8a10ec896d8
Patrick Hislop <pfh28@drexel.edu>
2016-06-15 22:02:31 -0400

    - draft getSpacer - finish removeLeadingSpaces - change structure of trimString

--- learnC/formatBasic.c
+++ learnC/formatBasic.c
@@ -6,9 +6,10 @@
 #include <stdlib.h>
 
 int getNumLines(FILE *inputFile);
-char* getSpacer(FILE *inputFile);
+int getSpacer(FILE *inputFile, char* spacer);
 int removeLeadingSpaces(FILE *inputFile);
 int fixSpacing(FILE *inputFile, char *spacer);
+char* trimString(char* string);
 
 int readsize = 1024;
 
@@ -21,9 +22,9 @@ int main()
 	inputFile = fopen("unformattedBasic.txt","r");
 	
 	lines = getNumLines(inputFile);
-	getSpacer(inputFile, spacer);
+	getSpacer(inputFile, spacer);					//not working
 	
-	removeLeadingSpaces(inputFile);
+	removeLeadingSpace(inputFile);
 	fixSpacing(inputFile, spacer);
 	
 	fclose(inputFile);
@@ -48,11 +49,19 @@ int getSpacer(FILE *inputFile, char* spacer)
 	return 0;
 }
 
-int removeLeadingSpaces(FILE *inputFile)
+int removeLeadingSpace(FILE *inputFile)
 {
-	FILE *outputFile = fopen("tmp.txt", "r");
+	FILE *outputFile = fopen("tmp.txt", "w");
+	char tmp[readsize];
+	char* trimmed;
 	
-	
+	do 
+	{
+		fgets(tmp, readsize, inputFile);
+		trimmed = trimString(tmp);
+		fputs(trimmed, outputFile);
+	}
+	while(!feof(inputFile));
 	
 	return 0;
 }
@@ -60,4 +69,17 @@ int removeLeadingSpaces(FILE *inputFile)
 int fixSpacing(FILE *inputFile, char *spacer)
 {
 	return 0;
+}
+
+char* trimString(char* string)
+{
+	//while(isspace(*string))					//for use with actual whitespace
+	//	string++;
+	 
+	while((string[0] == '·' || (string[0] == '»')))		//for use with the exercise
+	{
+		string++;
+	}
+	
+	return string;
 }
